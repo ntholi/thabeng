71cc097ed311bc412f4325a3da9d8257c006f29a
Ntholi Nkhatho <ntholi@live.com>
2024-05-17 21:01:12 +0200

    Update Firebase configuration to use environment variables

--- src/lib/config/firebase.ts
+++ src/lib/config/firebase.ts
@@ -1,25 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBU9lYkWOfKVWCJejhohkLRXxR4FMnwsFI',
-  authDomain: 'mamosetsels.firebaseapp.com',
-  projectId: 'mamosetsels',
-  storageBucket: 'mamosetsels.appspot.com',
-  messagingSenderId: '295490861497',
-  appId: '1:295490861497:web:90831ce4ca5ade68ba9e68',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
 
-export { db, auth, storage };
+export { db, storage, auth };
