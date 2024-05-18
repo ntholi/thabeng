da83da4d2a1448453ff63f9d222f07a788d6a0a4
Ntholi Nkhatho <ntholi@live.com>
2024-05-18 16:07:01 +0200

    setup nextui

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@nextui-org/react';
+
 export default function Home() {
   return (
     <main>
       <h1>Thabeng Hotel</h1>
+      <Button>Click Me</Button>
     </main>
   );
 }
