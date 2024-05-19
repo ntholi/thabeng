0ef8c17b9348daeeecb2e6ad603d98696b7ff58f
Ntholi Nkhatho <ntholi@live.com>
2024-05-19 14:35:04 +0200

    feat: Improve home page header with dark overlay and clickable cards

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,9 @@
 import { cn } from '@/lib/utils';
-import Image from 'next/image';
 import Link from 'next/link';
 import { MdFastfood as FastFood, MdCelebration, MdHotel } from 'react-icons/md';
+import { Josefin_Sans } from 'next/font/google';
+
+const josefinSans = Josefin_Sans({ subsets: ['latin'] });
 
 export default function Home() {
   const banner = '/images/test-image.jpg';
@@ -12,14 +14,20 @@ export default function Home() {
           backgroundImage: `url(${banner})`,
         }}
         className={cn(
-          'relative h-[92vh] bg-black/5 bg-cover bg-center text-white bg-blend-overlay',
+          'absolute h-[100vh] w-[100vw] top-0 bg-black/10 bg-cover bg-center text-white bg-blend-overlay',
           'flex flex-col justify-center items-center'
         )}
       >
-        <div className='p-52 dark-overlay'>
-          <h1 className='text-5xl'>Thabeng Hotel</h1>
+        <div className='h-56'>
+          <h1 className={cn(josefinSans.className, 'text-6xl font-bold')}>
+            Thabeng Hotel
+          </h1>
         </div>
-        <nav className='flex justify-center gap-5 py-6 bg-black/50 absolute w-full bottom-0 sm:mx-auto shadow-sm'>
+        <nav
+          className={cn(
+            'flex justify-center gap-5 py-6 backdrop-blur-sm bg-white/30 absolute w-full bottom-0 sm:mx-auto shadow-sm'
+          )}
+        >
           <ClickableCard icon={MdHotel} text='Hotel' />
           <ClickableCard icon={FastFood} text='Restaurant' />
           <ClickableCard icon={MdCelebration} text='Events' />
@@ -39,12 +47,12 @@ function ClickableCard({ icon: Icon, text }: ClickableCardProps) {
     <Link
       href='#'
       className={cn(
-        'rounded-md border bg-black/60 py-5 text-white',
-        'flex gap-2 w-48 justify-center'
+        'rounded-md border bg-white/80 py-4 text-black',
+        'flex gap-2 w-40 justify-center'
       )}
     >
-      <Icon className='text-xl sm:text-2xl' />
-      <span>{text}</span>
+      <Icon className='text-xl sm:text-xl' />
+      <span className='text-sm'>{text}</span>
     </Link>
   );
 }
