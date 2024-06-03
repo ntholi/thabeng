f70902c624dac9c6749413614f29ee248112c11f
Ntholi Nkhatho <ntholi@live.com>
2024-06-03 12:43:27 +0200

    feat: Refactor landing page to remove unused code and improve readability

--- src/app/(admin)/admin/home-page/page.tsx
+++ src/app/(admin)/admin/home-page/page.tsx
@@ -20,83 +20,89 @@ import ThemedButton from '../../admin-core/components/ThemedButton';
 import AutoArrange from './AutoArrange';
 
 export default function LandingPage() {
-  const [loading, setLoading] = useState(true);
-  const [articles, setArticles] = useState<Array<Post | null>>([]);
+  // const [loading, setLoading] = useState(true);
+  // const [articles, setArticles] = useState<Array<Post | null>>([]);
 
-  useEffect(() => {
-    return homePageRepository.listenArticles(setArticles);
-  }, []);
+  // useEffect(() => {
+  //   return homePageRepository.listenArticles(setArticles);
+  // }, []);
 
-  useEffect(() => {
-    if (articles.length) setLoading(false);
-  }, [articles]);
+  // useEffect(() => {
+  //   if (articles.length) setLoading(false);
+  // }, [articles]);
 
-  if (loading) {
-    return (
-      <Flex h={'100%'} w={'100%'} justify={'center'} align={'center'}>
-        <Loader />
-      </Flex>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <Flex h={'100%'} w={'100%'} justify={'center'} align={'center'}>
+  //       <Loader />
+  //     </Flex>
+  //   );
+  // }
 
   return (
-    <Box p={'md'}>
-      <Paper withBorder p={8} shadow='sm'>
-        <Paper p={'sm'}>
-          <Group justify='space-between'>
-            <Logo />
-            <AutoArrange />
-          </Group>
-        </Paper>
-        <Divider />
-        <Stack mt={'xl'}>
-          <Flex justify={'center'}>
-            <ArticleCard
-              index={0}
-              article={articles[0]}
-              w={{ base: '100%', sm: 300 }}
-              orientation='vertical'
-            />
-          </Flex>
-          <Flex justify={'center'} gap={'xl'} p={'xl'}>
-            {articles.slice(1, 4).map((it, i) => (
-              <ArticleCard
-                key={i}
-                index={i + 1}
-                article={it}
-                imageHeight={150}
-                w={{ base: '100%', sm: 200 }}
-                orientation='vertical'
-              />
-            ))}
-          </Flex>
-          <Divider mx={'lg'} />
-          <Flex justify={'center'} gap={'xl'} p={'xl'}>
-            {articles.slice(4, 8).map((it, i) => (
-              <ArticleCard
-                key={i}
-                index={i + 4}
-                article={it}
-                imageHeight={130}
-                w={{ base: '100%', sm: 200 }}
-                orientation='vertical'
-              />
-            ))}
-          </Flex>
-          <Divider mx={'lg'} />
-          <Stack px={80}>
-            {articles.slice(8, 11).map((it, i) => (
-              <ArticleCard
-                key={i}
-                index={i + 4}
-                article={it}
-                imageHeight={130}
-                orientation='horizontal'
-              />
-            ))}
-          </Stack>
-        </Stack>
-      </Paper>
+    <Box p='md'>
+      <Text ta='center'>Not Done Yet</Text>
     </Box>
   );
+
+  // return (
+  //   <Box p={'md'}>
+  //     <Paper withBorder p={8} shadow='sm'>
+  //       <Paper p={'sm'}>
+  //         <Group justify='space-between'>
+  //           <Logo />
+  //           <AutoArrange />
+  //         </Group>
+  //       </Paper>
+  //       <Divider />
+  //       <Stack mt={'xl'}>
+  //         <Flex justify={'center'}>
+  //           <ArticleCard
+  //             index={0}
+  //             article={articles[0]}
+  //             w={{ base: '100%', sm: 300 }}
+  //             orientation='vertical'
+  //           />
+  //         </Flex>
+  //         <Flex justify={'center'} gap={'xl'} p={'xl'}>
+  //           {articles.slice(1, 4).map((it, i) => (
+  //             <ArticleCard
+  //               key={i}
+  //               index={i + 1}
+  //               article={it}
+  //               imageHeight={150}
+  //               w={{ base: '100%', sm: 200 }}
+  //               orientation='vertical'
+  //             />
+  //           ))}
+  //         </Flex>
+  //         <Divider mx={'lg'} />
+  //         <Flex justify={'center'} gap={'xl'} p={'xl'}>
+  //           {articles.slice(4, 8).map((it, i) => (
+  //             <ArticleCard
+  //               key={i}
+  //               index={i + 4}
+  //               article={it}
+  //               imageHeight={130}
+  //               w={{ base: '100%', sm: 200 }}
+  //               orientation='vertical'
+  //             />
+  //           ))}
+  //         </Flex>
+  //         <Divider mx={'lg'} />
+  //         <Stack px={80}>
+  //           {articles.slice(8, 11).map((it, i) => (
+  //             <ArticleCard
+  //               key={i}
+  //               index={i + 4}
+  //               article={it}
+  //               imageHeight={130}
+  //               orientation='horizontal'
+  //             />
+  //           ))}
+  //         </Stack>
+  //       </Stack>
+  //     </Paper>
+  //   </Box>
+  // );
 }
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -29,14 +29,19 @@ export default function Home() {
       >
         <div
           className={cn(
-            'h-56 text-center dark-overlay px-20 py-5',
+            'h-56 text-center dark-overlay px-6 sm:px-20 py-5',
             'flex flex-col justify-between'
           )}
         >
-          <h1 className={cn(josefinSans.className, 'text-7xl font-bold')}>
+          <h1
+            className={cn(
+              josefinSans.className,
+              'text-5xl sm:text-7xl font-bold'
+            )}
+          >
             Thabeng Hotel
           </h1>
-          <article className='flex flex-col items-center gap-2'>
+          <article className='flex flex-col items-center gap-5 my-5'>
             <p>
               Welcome to Thabeng Hotel, where you can find the best services for
               your stay.
@@ -55,7 +60,7 @@ export default function Home() {
         <nav
           className={cn(
             'absolute w-full bottom-5',
-            'flex justify-center gap-5 py-6 w-[60vw] sm:mx-auto',
+            'flex justify-center gap-5 px-2 py-6 sm:w-[60vw] sm:mx-auto',
             'backdrop-blur-sm bg-white/30',
             'rounded-t-lg sm:rounded-lg'
           )}
@@ -79,12 +84,12 @@ function ClickableCard({ icon: Icon, text }: ClickableCardProps) {
     <Link
       href='#'
       className={cn(
-        'rounded-md bg-blue-900/80 py-4 text-white',
-        'flex gap-2 w-40 justify-center'
+        'rounded-md bg-blue-900/80 py-4 px-4 text-white',
+        'flex gap-2 sm:w-40 justify-center'
       )}
     >
-      <Icon className='text-xl sm:text-xl' />
-      <span className='text-sm'>{text}</span>
+      <Icon className='text-xl sm:text-xl hidden sm:block' />
+      <span className='text-xs sm:text-sm'>{text}</span>
     </Link>
   );
 }
