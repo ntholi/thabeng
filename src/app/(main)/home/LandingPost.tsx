68e7298d0aae146328eb44e8c0fd7d101b010f7d
Ntholi Nkhatho <ntholi@live.com>
2024-06-16 18:31:24 +0200

    update landing post

--- src/app/(main)/home/LandingPost.tsx
+++ src/app/(main)/home/LandingPost.tsx
@@ -1,20 +1,28 @@
+import { homePageRepository } from '@/app/(admin)/admin/home-page/repository';
 import { Button, Skeleton } from '@nextui-org/react';
 import { IconArrowRight } from '@tabler/icons-react';
-import React, { useState } from 'react';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 
 export default function LandingPost() {
-  const [post, setPost] = useState<HomePage>();
-  const [loading, setLoading] = useState();
+  const [post, setPost] = useState<HomePage | null>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    homePageRepository
+      .getPost()
+      .then(setPost)
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <article className='flex flex-col items-center gap-5 my-5'>
       {loading ? (
         <Skeleton className='h-5 w-24 rounded-sm' />
       ) : (
-        <p>
-          Welcome to Thabeng Hotel, where you can find the best services for
-          your stay.
-        </p>
+        <Link href={`/posts/${post?.value}`}>{post?.label}</Link>
       )}
       <Button
         isDisabled={loading}
