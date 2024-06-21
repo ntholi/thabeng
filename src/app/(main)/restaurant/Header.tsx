79bec0e0be86e5ef4b4b04cbbfd484c13b93e46a
Ntholi Nkhatho <ntholi@live.com>
2024-06-21 19:57:44 +0200

    chore: Update npm dependencies to include prettier and prettier-plugin-tailwindcss

--- src/app/(main)/restaurant/Header.tsx
+++ src/app/(main)/restaurant/Header.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 export default function Header() {
-  const banner = '/images/test-image.jpg';
+  const banner = "/images/test-image.jpg";
 
   return (
-    <div className='relative'>
+    <div className="relative">
       <Image
         src={banner}
         width={1920}
         height={1080}
-        alt='Banner'
-        className='h-[50vh] object-cover w-full'
+        alt="Banner"
+        className="h-[50vh] w-full object-cover"
       />
-      <div className='text-white text-center bg-black/30 absolute inset-0 h-full px-6 sm:px-20 py-5 flex flex-col justify-between'>
-        <h1 className='text-5xl sm:text-7xl font-bold'>Thabeng Hotel</h1>
-        <p className='text-white text-xl'>
+      <div className="absolute inset-0 flex h-full flex-col justify-between bg-black/30 px-6 py-5 text-center text-white sm:px-20">
+        <h1 className="text-5xl font-bold sm:text-7xl">Thabeng Hotel</h1>
+        <p className="text-xl text-white">
           Savor exquisite flavors with our diverse and delightful menu.
         </p>
       </div>
