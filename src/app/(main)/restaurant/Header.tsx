17f4413e2f57f346cad62f898197ed78ef6668a6
Ntholi Nkhatho <ntholi@live.com>
2024-06-23 13:17:21 +0200

    feat: Add NumberField component for price in MealTimePage

--- src/app/(main)/restaurant/Header.tsx
+++ src/app/(main)/restaurant/Header.tsx
@@ -1,18 +1,20 @@
-import React, { Suspense } from 'react';
-import Image from 'next/image';
-import { restaurantPageRepository } from '@/app/(admin)/admin/restaurant/page/repository';
-import { mealtimeRepository } from '@/app/(admin)/admin/restaurant/meal-times/repository';
-import { Spinner } from '@nextui-org/react';
 import { MealTime } from '@/app/(admin)/admin/restaurant/meal-times/MealTime';
+import { mealtimeRepository } from '@/app/(admin)/admin/restaurant/meal-times/repository';
+import { restaurantPageRepository } from '@/app/(admin)/admin/restaurant/page/repository';
+import { cn } from '@/lib/utils';
+import { Spinner } from '@nextui-org/react';
 import {
   IconBread,
-  IconDogBowl,
   IconEggCracked,
   IconLollipop,
-  IconMeat,
   IconSalad,
   IconSoup,
 } from '@tabler/icons-react';
+import { Salsa } from 'next/font/google';
+import Image from 'next/image';
+import React, { Suspense } from 'react';
+
+const font = Salsa({ weight: '400', subsets: ['latin'] });
 
 export default async function Header() {
   const page = await restaurantPageRepository.getRestaurantPage();
@@ -25,11 +27,13 @@ export default async function Header() {
           width={1920}
           height={1080}
           alt='Banner'
-          className='h-[55vh] w-full object-cover'
+          className='h-[60vh] w-full object-cover'
         />
         <div className='absolute inset-0 flex h-full flex-col items-center justify-center bg-black/30 px-6 py-5 text-white'>
-          <h1 className='text-5xl font-bold sm:text-7xl'>{page?.heading}</h1>
-          <p className='text-xl'>{page?.subheading}</p>
+          <h1 className={cn(font.className, 'text-5xl font-bold sm:text-7xl')}>
+            {page?.heading}
+          </h1>
+          <p className='mt-2'>{page?.subheading}</p>
         </div>
       </header>
       <Suspense
@@ -48,18 +52,23 @@ export default async function Header() {
 async function MealTimeDisplay() {
   const mealTimes = await mealtimeRepository.getAll();
   return (
-    <section className='-top-24 mx-auto flex w-full bg-blue-900/95 text-white md:relative md:w-[60vw]'>
-      {mealTimes.map((it) => (
-        <div
-          key={it.id}
-          className='flex flex-1 flex-col items-center gap-2 py-14'
-        >
-          <Icon mealName={it.name} />
-          <h4 className='text-sm'>{it.name}</h4>
-          <p className='text-xs'>
-            {it.startTime} - {it.endTime}
-          </p>
-        </div>
+    <section className='-top-28 mx-auto flex w-full bg-blue-900/95 text-white md:relative md:w-[60vw]'>
+      {mealTimes.map((it, i) => (
+        <React.Fragment key={it.id}>
+          <div className='flex flex-1 flex-col items-center gap-1 p-4 sm:gap-2 sm:px-5 sm:py-10'>
+            <Icon mealName={it.name} />
+            <h4>{it.name}</h4>
+            <p className='text-xs sm:text-sm'>
+              {it.startTime} - {it.endTime}
+            </p>
+            <p className='hidden text-center text-xs sm:block'>
+              {it.description}
+            </p>
+          </div>
+          {i < mealTimes.length - 1 && (
+            <div className='border-r border-zinc-300' />
+          )}
+        </React.Fragment>
       ))}
     </section>
   );
