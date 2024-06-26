b5271769764825ff123cc8588d998e9b479f962e
Ntholi Nkhatho <ntholi@live.com>
2024-06-26 14:29:40 +0200

    ```text refactor: Add empty space below content in RootLayout component
    The code changes in the RootLayout component add an empty div with a height of 10vh below the content. This provides additional spacing and improves the visual presentation of the layout.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           {children}
+          <div className='h-[10vh]'></div>
           <Footer />
         </Providers>
       </body>
